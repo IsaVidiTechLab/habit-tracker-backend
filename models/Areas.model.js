90240d64b51d855c11a7168598e8b04aa2aded16
Vidisha Shah <vidisha.shah14@gmail.com>
2024-08-14 15:15:41 +0200

    Areas model created

--- models/Areas.model.js
+++ models/Areas.model.js
@@ -1,1 +1,16 @@
 const { Schema, model } = require("mongoose");
+
+const areaSchema = new Schema({
+    areaName: {
+      type: String,
+      required: [true, "Name is required."],
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required : true
+    }
+})
+
+const Areas = model ("Areas", areaSchema);
+model.exports = Areas;
